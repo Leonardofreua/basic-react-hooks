bec64e3fa62d5040322a7d15c519cb83f1e7c6d3
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-15 18:19:58 -0300

    Moved the hooks to App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
-
-import Route from './routes';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return <Route />;
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  function handleAdd() {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  // componentDidMount - dependency array is empty
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('techs');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+
+    // Only componentWillAmount
+    // return () => {};
+  }, []);
+
+  // componentDidUpdate - monitoring -- techs state --
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  return (
+    <>
+      <ul>
+        {techs.map((tech) => (
+          <li key={tech}>{tech}</li>
+        ))}
+      </ul>
+      <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Add
+      </button>
+    </>
+  );
 }
 
 export default App;
